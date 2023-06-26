b8d3b59a6dd57cda56a6d9e2506c9d026aa29056
prathamfzr02@gmail.com <prathamfzr02@gmai.com>
2023-06-26 13:04:44 +0530

    Frontend

---
+++ src/data.js
@@ -0,0 +1,80 @@
+export const sliderItems = [
+
+    {
+        id:1,
+        img: 'https://i.ibb.co/BwtzXVy/pic2.jpg',
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd",
+    },
+    {
+        id:2,
+        img: 'https://i.ibb.co/X5nnhy1/pic1.jpg',
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fcf1ed",
+    },
+    {
+        id:3,
+        img: 'https://i.ibb.co/9sW1hyK/pic3.jpg',
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+
+    {
+        id: 1,
+        img: 'https://i.ibb.co/TTsd6Vj/shirt-pic.jpg',
+        title: "SHIRT STYLE", 
+    },
+
+    {
+        id: 2,
+        img: 'https://i.ibb.co/nLNnfpV/loungewear-pic.jpg',
+        title: "LOUNGEWEAR LOVE", 
+    },
+
+    {
+        id: 3,
+        img: 'https://i.ibb.co/xzJ2RBj/jacket-pic.jpg',
+        title: "LIGHT JACKETS", 
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: 'https://i.ibb.co/58HMZNt/airmax.jpg',
+    },
+    {
+        id: 2,
+        img: 'https://i.ibb.co/VJsfF6W/airforce.jpg',
+    },
+    {
+        id: 3,
+        img: 'https://i.ibb.co/YcC362d/printed-shirt.jpg',
+    },
+    {
+        id: 4,
+        img: 'https://i.ibb.co/qYs0RPJ/puma-hoodie.jpg',
+    },
+    {
+        id: 5,
+        img: 'https://i.ibb.co/kXQvmkw/backprintedtee-pic.jpg',
+    },
+    {
+        id: 6,
+        img: 'https://i.ibb.co/q0MxV7X/hoodie.jpg',
+    },
+    {
+        id: 7,
+        img: 'https://i.ibb.co/kXQvmkw/backprintedtee-pic.jpg',
+    },
+    {
+        id: 8,
+        img: 'https://i.ibb.co/q0MxV7X/hoodie.jpg',
+    },
+]
